d87f2c8e71f47a26a36ac04d9a2e060f5798ddf9
Juleinn <juleintartem@hotmail.fr>
2017-06-10 18:21:51 +0200

    resolved conflicts

--- printer/site/js/printer_order.js
+++ printer/site/js/printer_order.js
@@ -1,17 +1,6 @@
 $(document).ready(function(){	
 	
-<<<<<<< HEAD
-    $("#print_button").click(function(){
-    	topSlice = $("#topslice_selector").children("li.uk-active").text()
-    	topGarnish = $("#topgarnish_selector").children("li.uk-active").text()
-    	bottomGarnish = $("#bottomgarnish_selector").children("li.uk-active").text()
-    	bottomSlice	 = $("#bottomslice_selector").children("li.uk-active").text()
-    	if(confirm("Confirm sandwich : " + topSlice + "," + topGarnish + "," + bottomGarnish + "," + bottomSlice  + "?"))
-    	{
-    		$.post("/print", {
-    			"test sandwich": true
-    		})
-=======
+
 	$("#print_button").click(function(){
 		topSlice = $("#topslice_selector").children("li.uk-active").text()
 		topGarnish = $("#topgarnish_selector").children("li.uk-active").text()
@@ -45,7 +34,6 @@ $(document).ready(function(){
 
     		$.post("/print", (new XMLSerializer().serializeToString(xmlDocument.documentElement)));
 
->>>>>>> d0e3b9a22f34564b57a7ca07bfd3c9efb3ddb8ba
     	}
     });
 });
